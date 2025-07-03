82ccd8a6bb2aa84b198c2143f5d31af5b09c59a2
leonardo-eyer <leonardo.eyer@hotmail.com>
2025-07-02 22:54:44 -0300

    login

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -2,16 +2,30 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from blueprints.app import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__, template_folder="templates")
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            flash("Logged in", category="success")
+            login_user(user, remember=True)
+            return redirect(url_for("core.index"))
+
+        flash("Incorrect password or email", category="error")
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>logout</h1>"
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 @auth.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -23,6 +37,8 @@ def signup():
         password2 = request.form['password2']
         if len(email) < 4:
             flash("Email must be at least 4 characters", category="error")
+        elif User.query.filter_by(email=email).first():
+            flash("Email already registered", category="error")
         elif len(firstname) < 2:
             flash("First name must be at least 2 characters", category="error")
         elif len(password1) < 8:
@@ -37,7 +53,8 @@ def signup():
             )
             db.session.add(user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Registration successful", category="success")
             return redirect(url_for('core.index'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
--- blueprints/core/routes.py
+++ blueprints/core/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 core = Blueprint("core", __name__, template_folder="templates")
 
 @core.route('/')
+@login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
