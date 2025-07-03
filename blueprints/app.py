a0d5c08000c2d10a033cb29bec10130da89c8c7d
leonardo-eyer <leonardo.eyer@hotmail.com>
2025-07-02 23:31:49 -0300

    add and delete notes

--- blueprints/app.py
+++ blueprints/app.py
@@ -12,11 +12,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DB_NAME
     db.init_app(app)
 
-    from blueprints.views.routes import views
     from blueprints.auth.routes import auth
     from blueprints.core.routes import core
 
-    app.register_blueprint(views, url_prefix="/views")
     app.register_blueprint(auth, url_prefix="/auth")
     app.register_blueprint(core, url_prefix="")
 
--- blueprints/core/routes.py
+++ blueprints/core/routes.py
@@ -1,9 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
+from .models import Note
+from blueprints.app import db
+import json
 
 core = Blueprint("core", __name__, template_folder="templates")
 
-@core.route('/')
+@core.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    if request.method == "POST":
+        note = request.form["note"]
+        if len(note) <1:
+            flash("note is too short", category="error")
+        else:
+            new_note = Note(text=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("new note created", category="success")
     return render_template("index.html", user=current_user)
+
+@core.route("delete-note", methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note["noteId"]
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+
+    return jsonify({})
